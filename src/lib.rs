1ef2a86650258c3a306b92eb295b932002cb790a
Zahari Petkov <zahari@balena.io>
2022-08-23 15:48:17 +0300

    Enable verbose clippy warnings
    Change-type: patch
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+#![warn(
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo,
+    rust_2018_idioms,
+    rust_2018_compatibility,
+    rust_2021_compatibility,
+    future_incompatible,
+    nonstandard_style,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    unused
+)]
+
 mod checksum;
 mod copy;
 mod fs;
